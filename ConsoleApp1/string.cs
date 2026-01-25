9984ef451d9302451576f3d6d171e6e9fa465f8f
raspu_iu80@student.itstep.org <raspu_iu80@student.itstep.org>
2026-01-25 19:14:04 +0200

    String

---
+++ ConsoleApp1/string.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ex5");
+            string text = "i like programming on sharp";
+            string[] words = text.Split(new char[] { ' ', '.', '-', '?', '!' }, StringSplitOptions.RemoveEmptyEntries);
+            char letter = words[1][0];
+            Console.WriteLine(letter);
+
+            Console.WriteLine("Ex6");
+            foreach(var word in words)
+            { Console.Write(word + "*"); }
+
+            Console.WriteLine();
+            Console.WriteLine("Ex7");
+            StringBuilder sb = new StringBuilder();
+            string word2;
+            Console.Write("Enter the word(place fullstop to stop the sentence): ");
+            do
+            {
+                word2 = Console.ReadLine();
+                sb.Append(word2 + ", ");
+            }
+            while (!word2.Contains("."));
+            sb.Length -= 2;
+            Console.WriteLine(sb);
+        }
+    }
+}
