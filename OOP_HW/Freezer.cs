9ec28fe2b0a8098ba596f8d742f18d24e26067b9
raspu_iu80@student.itstep.org <raspu_iu80@student.itstep.org>
2026-01-27 12:49:13 +0200

    OOP

---
+++ OOP_HW/Freezer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_HW
+{
+    partial class Freezer
+    {
+        static Freezer()
+        {
+            total = 0;
+            maxtemp = -18.0;
+        }
+        public Freezer()
+        {
+            brand = "Unknown";
+            capL = 100;
+            manuDate = DateTime.Now;
+            isOn = false;
+            temp = maxtemp;
+
+            total++;
+        }
+        public Freezer(string brand, int capacityLiters)
+        {
+            this.brand = brand;
+            this.capL = capacityLiters;
+            manuDate = DateTime.Now;
+            isOn = false;
+            temp = maxtemp;
+
+            total++;
+        }
+        public Freezer(string brand, int capacityLiters, DateTime manufactureDate, bool isOn)
+        {
+            this.brand = brand;
+            this.capL = capacityLiters;
+            this.manuDate = manufactureDate;
+            this.isOn = isOn;
+            this.temp = maxtemp;
+
+            total++;
+        }
+
+        
+        public override string ToString()
+        {
+            return $"Brand: {brand}, " +
+                   $"Capacity: {capL} л, " +
+                   $"Temperature: {temp} °C, " +
+                   $"State: {(isOn ? "On" : "Off")}, " +
+                   $"Manufacture Date: {manuDate.ToShortDateString()}";
+        }
+    }
+}
---
+++ OOP_HW/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_HW
+{
+
+    partial class Freezer
+    {
+        private string brand;            
+        private int capL;       
+        private double temp;       
+        private bool isOn;
+        private DateTime manuDate;
+
+        private static int total = 0; // Кількість створених обʼєктів
+        private static double maxtemp = -18.0; // Макс. допустима температура
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Freezer[] freezers = new Freezer[5];
+
+            freezers[0] = new Freezer();
+            freezers[1] = new Freezer("LG", 180);
+            freezers[2] = new Freezer("Bosch", 220, new DateTime(2022, 11, 1), true);
+            freezers[3] = new Freezer("Whirlpool", 195, new DateTime(2019, 7, 20), false);
+            freezers[4] = new Freezer("Electrolux", 210, new DateTime(2023, 2, 15), true);
+
+            foreach (Freezer freezer in freezers)
+            {
+                Console.WriteLine(freezer.ToString());
+            }
+        }
+    }
+}
